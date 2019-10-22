adbab87af33e5bb157cb7e8fa3442c9fdcb0d1c8
riki1 <rikipaleos@gmail.com>
2019-10-22 17:55:14 +0200

    html.php

---
+++ html.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+	<title>Ricardo</title>
+</head>
+<body>
+<center>
+<h1>SANTANDER</h1>
+</center>
+Ciudad de 175.000 personas </br> <br/>
+Mi nombre: Ricardo Gallo
+<h2>Ciudad donde vivo</h2>
+<h3>Tiene 5 playas</h3>
+Sardinero, La virgen del mar, Bikinis, El camello, San juan
+
+<p> <b> <u>Santander</b> </u> es una ciudad pequeña y tranquila, esta al <b>norte</b> asi que hace frio que le vamos a hacer, por lo demas esta bastante bien.</p> <br/>
+
+En santander hay zonas muy turísticas, y cuándo llega el verano, se llena de guiris. Una zona muy visitada por los turistas es la magdalena, se siempre hay peña.
+</br> <br/>
+
+<img src="santander.jpg" width="550" height="350" /> <br/>
+
+</br>
+
+<form>
+	<div>
+		<input type="search" id="miBusqueda" name="q"
+		placeholder="Buscar en el sitio...">
+		<button>Buscar</button>
+	</div>
+</form>
+</br> <br/>
+<TABLE BORDER>
+		<TR><TH>CANTABRIA</TH>
+				<TD>Santander</TD> <TD>Palencia<TD> <TD>Burgos</TD></TR> <TR><TH>ASTURIAS</TH>
+				<TD>Gijon</TD> <TD>Oviedo<TD> <TD>Avilés</TD></TR> <TR><TH>GALICIA</TH>
+				<TD>Lugo</TD> <TD>La coruña<TD> <TD>Orense</TD></TR>
+</TABLE>
+</br> <br/>
+<center>
+<font face="Comic Sans MS,arial,verdana"> <font size=25> <font color="red">KINDER BUENO</font> </font> </font>
+</center>
+
+</body>
+</html>
+	
